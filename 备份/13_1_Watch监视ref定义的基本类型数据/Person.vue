9effa3a7e9d53a9cf021e11254f8af2917802c0f
shipeng <1677609546@qq.com>
2023-12-25 21:30:14 +0800

    watch

---
+++ 备份/13_1_Watch监视ref定义的基本类型数据/Person.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="person">
+    <h1></h1>
+    <h2>当前求和为: {{ sum }}</h2>
+    <button @click="changeSum">点我 sum +1</button>
+  </div>
+</template>
+<script setup lang="ts" name="Arc">
+import { ref, watch } from "vue";
+let sum = ref(0);
+function changeSum() {
+  sum.value += 1;
+}
+// watch(监视谁?,回调函数)
+//监视,情况一:监视(ref)定义的(基本类型|)数据
+const stopWatch = watch(sum, (newValue, oldValue) => {
+  console.log(`${newValue},${oldValue},`);
+  if (newValue > 10) {
+    //当sum>10结束监视
+    stopWatch();
+  }
+});
+</script>
+<style scoped>
+.person {
+  background-color: #aff;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button {
+  margin-right: 10px;
+}
+input {
+  margin-bottom: 10px;
+}
+</style>
--- 备份/13_2_Watch监视ref对象/Person.vue
+++ 备份/13_2_Watch监视ref对象/Person.vue
@@ -1,45 +1,35 @@
 <template>
   <div class="person">
-    <h1>情况三: 监视[reactive]对象</h1>
+    <h1>情况二: 监视[ref]对象</h1>
     <h2>姓名: {{ person.name }}</h2>
     <h2>年龄: {{ person.age }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
     <button @click="changePerson">修改整个人</button>
-    <hr />
-    <h1>{obj.a.b.c}</h1>
   </div>
 </template>
 <script setup lang="ts" name="Arc">
-import { reactive, watch } from "vue";
-let person = reactive({
+import { ref, watch } from "vue";
+let person = ref({
   name: "张三",
   age: 18,
 });
-let obj = reactive({
-  a: {
-    b: {
-      c: 666,
-    },
-  },
-});
 function changeName() {
-  person.name += "~";
+  person.value.name += "~";
 }
 function changeAge() {
-  person.age += 1;
+  person.value.age += 1;
 }
-//默认开启深度监视
 function changePerson() {
-  Object.assign(person, { name: "里斯", age: 17 });
-} //修改reactive相对于ref 是在原来的对象修改的所以newValue=oldValue
-//监视的是地址 如果地址没有变化则newValue=oldValue
-watch(person, (newValue, oldValue) => {
-  console.log(newValue, oldValue);
-});
-function test() {
-  obj.a.b.c = 19;
+  person.value = { name: "list", age: 19 };
 }
+watch(
+  person,
+  (newValue, oldValue) => {
+    console.log(newValue, oldValue);
+  },
+  { deep: true, immediate: true }
+);
 </script>
 <style scoped>
 .person {
